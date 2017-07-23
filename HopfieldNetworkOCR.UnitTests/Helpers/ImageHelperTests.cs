10ab4df5f0b671b36895f5e3ed0aeef01e0ccb52
Draeggiar <tama2@wp.pl>
2017-07-23 14:52:16 +0200

    Image Loadning and Saving + Tests

--- HopfieldNetworkOCR.UnitTests/Helpers/ImageHelperTests.cs
+++ HopfieldNetworkOCR.UnitTests/Helpers/ImageHelperTests.cs
@@ -1,24 +1,69 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.IO;
 
 namespace HopfieldNetworkOCR.UnitTests.Helpers
 {
     [TestClass]
-    [TestCategory("Helpers")]
     public class ImageHelperTests
     {
+        private const string _imageFolder = "../../img/";
+        private const string _imageName = "A.tiff";
+
         [TestMethod]
+        [TestCategory("Helpers")]
         public void LoadImageTest()
         {
-            Assert.Fail();
+            Image img = Image.FromFile(_imageFolder + _imageName);
+            byte[] good;
+            using (var  ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+                good = ms.ToArray();
+            }
+
+            byte [] test = HopfieldNetworkOCR.Helpers.ImageHelper.LoadImage(_imageFolder + _imageName);
+
+            Assert.AreEqual(good.Length, test.Length);
+
+            for (int i=0; i<test.Length; i++)
+            {
+                Assert.AreEqual(good[i], test[i]);
+            }
         }
 
         [TestMethod]
+        [TestCategory("Helpers")]
         public void SaveImageTest()
-        {
-            Assert.Fail();
+        {           
+            byte[] test;
+            using (var ms = new MemoryStream())
+            {
+                var img = Image.FromFile(_imageFolder + _imageName);
+
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+                test = ms.ToArray();
+            }
+
+            HopfieldNetworkOCR.Helpers.ImageHelper.SaveImage(test, _imageFolder + "save_test.tiff");
+
+            byte[] result;
+            using (var ms = new MemoryStream())
+            {
+                var img = Image.FromFile(_imageFolder + "save_test.tiff");
+
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+                result = ms.ToArray();
+            }
+
+            Assert.AreEqual(test.Length, result.Length);
+
+            for (int i = 0; i < test.Length; i++)
+                Assert.AreEqual(test[i], result[i]);           
         }
 
         [TestMethod]
+        [TestCategory("Helpers")]
         public void LoadAllFromCatalogTest()
         {
             Assert.Fail();
--- HopfieldNetworkOCR/Helpers/ImageHelper.cs
+++ HopfieldNetworkOCR/Helpers/ImageHelper.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 
 namespace HopfieldNetworkOCR.Helpers
 {
     public static class ImageHelper
     {
-        public static byte[] LoadImage(string image)
+        public static byte[] LoadImage(string imagePath)
         {
-            throw new System.NotImplementedException();
+            var img = Image.FromFile(imagePath);
+            byte[] bytes;
+
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+                bytes = ms.ToArray();
+            }
+
+            return bytes;
         }
 
-        public static void SaveImage(byte [] imageStream)
+        public static void SaveImage(byte [] imageStream, string imagePath)
         {
-            throw new System.NotImplementedException();
+            using (var ms = new MemoryStream(imageStream))
+            {
+                var img = Image.FromStream(ms);
+                img.Save(imagePath, System.Drawing.Imaging.ImageFormat.Tiff);
+            }
         }
 
         public static List<byte[]> LoadAllFromCatalog(string path)
